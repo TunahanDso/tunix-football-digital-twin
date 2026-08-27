45d55b689b15527b53064ad88ec12228dcb6d5ff
TunahanDso <89835977+TunahanDso@users.noreply.github.com>
2026-08-27 12:51:37 +0300

    style: sort Alembic imports

--- alembic/env.py
+++ alembic/env.py
@@ -4,11 +4,11 @@ import asyncio
 import os
 from logging.config import fileConfig
 
-from alembic import context
 from sqlalchemy import pool
 from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
 
+from alembic import context
 from tunix_football.db import models as _models  # noqa: F401
 from tunix_football.db.base import Base
 
