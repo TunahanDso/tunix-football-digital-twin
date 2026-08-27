ebbbfdf9364ce6ee2fe58529cb6874b07e140f81
TunahanDso <89835977+TunahanDso@users.noreply.github.com>
2026-08-27 13:34:16 +0300

    style(modeling): use collections abc sequence

--- src/tunix_football/modeling/models.py
+++ src/tunix_football/modeling/models.py
@@ -1,9 +1,9 @@
 from __future__ import annotations
 
+from collections.abc import Sequence
 from dataclasses import dataclass
 from datetime import datetime
 from math import exp, factorial
-from typing import Sequence
 from uuid import UUID
 
 from tunix_football.modeling.types import (
